441be1aa407733e2dca2f4155622b05a50354eee
soumesh2002 <soumesh6622@gmail.com>
2022-07-31 13:26:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Sockets;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter the name of server: ");
+        string? server = Console.ReadLine(); // discarded symbol
+        TcpClient client = new TcpClient(server, 80); // 80 -> web port of HTTP
+        StreamReader sr = new StreamReader(client.GetStream());
+        StreamWriter sw = new StreamWriter(client.GetStream());
+
+        try
+        {
+            sw.WriteLine("GET / HTTP/1.0\n\n");
+            sw.Flush();
+
+            string data = sr.ReadLine();
+            while (data != null)
+            {
+                Console.WriteLine(data);
+                data = sr.ReadLine();
+            }
+            client.Close();
+        }
+        catch (Exception)
+        {
+
+        }
+    }
+}
